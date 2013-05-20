b40248f96007b45cb97397c9c5459a956914555c
dupoxy <dupoxy@users.noreply.github.com>
2013-05-20 22:06:07 +0200

    update to actual tour

---
+++ gotour/appengine.go
@@ -0,0 +1,78 @@
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build appengine
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"net/http"
+
+	"appengine"
+
+	_ "code.google.com/p/go.talks/pkg/playground"
+)
+
+const runUrl = "http://golang.org/compile"
+
+func init() {
+	http.HandleFunc("/", rootHandler)
+	err := serveScripts("js", "playground.js")
+	if err != nil {
+		panic(err)
+	}
+	if err := initTour("."); err != nil {
+		panic(err)
+	}
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	if err := renderTour(w); err != nil {
+		c.Criticalf("template render: %v", err)
+	}
+}
+
+// prepContent returns a Reader that produces the content from the given
+// Reader, but strips the prefix "#appengine: " from each line. It also drops
+// any non-blank like that follows a series of 1 or more lines with the prefix.
+func prepContent(in io.Reader) io.Reader {
+	var prefix = []byte("#appengine: ")
+	out, w := io.Pipe()
+	go func() {
+		r := bufio.NewReader(in)
+		drop := false
+		for {
+			b, err := r.ReadBytes('\n')
+			if err != nil && err != io.EOF {
+				w.CloseWithError(err)
+				return
+			}
+			if bytes.HasPrefix(b, prefix) {
+				b = b[len(prefix):]
+				drop = true
+			} else if drop {
+				if len(b) > 1 {
+					b = nil
+				}
+				drop = false
+			}
+			if len(b) > 0 {
+				w.Write(b)
+			}
+			if err == io.EOF {
+				w.Close()
+				return
+			}
+		}
+	}()
+	return out
+}
+
+// socketAddr returns the WebSocket handler address.
+// The App Engine version does not provide a WebSocket handler.
+func socketAddr() string { return "" }
---
+++ js/lang.js
@@ -0,0 +1,18 @@
+// Localized user interface.
+var tr = {
+	"off": "off",
+	"on": "on",
+	"syntax": "coloration syntaxique",
+	"lineno": "Line-Numbers",
+	"reset": "Réinitialiser Slide",
+	"format": "Format Source Code",
+	"kill": "Tuer Programme",
+	"run": "Exécuter",
+	"compile": "Compiler et Exécuter",
+	"more": "Options",
+	"toc": "Table des matières",
+	"prev": "Précédent",
+	"next": "Suivant",
+	"waiting": "En attente du serveur distant...",
+	"errcomm": "Erreur de communication avec le serveur distant.",
+};
