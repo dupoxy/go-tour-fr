4990c3328604c4ecaf10007fe2d9fa5e35637c45
dupoxy <dupoxy@gmail.com>
2014-05-09 12:28:25 +0200

    sync all repository to Revision: dc166fa1dd14 of go-tour

--- content/prog/tour/sandbox.go
+++ content/prog/tour/sandbox.go
@@ -11,10 +11,4 @@ func main() {
 	fmt.Println("Welcome to the playground!")
 
 	fmt.Println("The time is", time.Now())
-
-	fmt.Println("And if you try to open a file:")
-	fmt.Println(os.Open("filename"))
-
-	fmt.Println("Or access the network:")
-	fmt.Println(net.Dial("tcp", "google.com"))
 }
